b1520b5c6ea9bdcd0737149fbf7e065b41e0657d
Archit Sharma <archit0030@gmail.com>
2024-09-24 15:27:08 +0530

    new

--- Task5_autonomus_picking/Pick and pour/inverse_kinematics_2.py
+++ Task5_autonomus_picking/Pick and pour/inverse_kinematics_2.py
@@ -9,6 +9,8 @@ from std_msgs.msg import Float32MultiArray
 from cv_bridge import CvBridge, CvBridgeError
 import moveit_commander
 import sys
+# sys.path.append('/home/surya/catkin_ws/src/Task5_autonomus_picking/scripts') # add path if needed
+from get_transform2 import *
 
 class DepthTo3DConverter:
     def __init__(self):
@@ -115,43 +117,6 @@ class DepthTo3DConverter:
                 rospy.logwarn("Transform not available")
         else:
             rospy.logwarn("Waiting for coordinates")
-
-    def get_transform(self, target_frame, source_frame):
-        try:
-            transform = self.tf_buffer.lookup_transform(target_frame, source_frame, rospy.Time(0), rospy.Duration(1.0))
-            return transform
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rospy.logwarn(f"Failed to get transform from {source_frame} to {target_frame}")
-            return None
-
-    def transform_to_matrix(self, transform):
-        translation = transform.transform.translation
-        rotation = transform.transform.rotation
-        print(translation.y,translation.x)
-        translation_matrix = np.array([
-            [1, 0, 0, translation.x],
-            [0, 1, 0, translation.y],
-            [0, 0, 1, translation.z],
-            [0, 0, 0, 1]
-        ])
-
-        quaternion = [rotation.x, rotation.y, rotation.z, rotation.w]
-        rotation_matrix = tf.transformations.quaternion_matrix(quaternion)
-        transformation_matrix = np.dot(translation_matrix, rotation_matrix)
-        return transformation_matrix
-
-
-    def transform_coordinates(self, object_coordinates_camera_frame, transformation_matrix):
-        # Add a homogeneous coordinate (1) to the object coordinates
-        object_coordinates_camera_frame_homogeneous = np.append(object_coordinates_camera_frame, 1)
-
-        # Perform the coordinate transformation
-        object_coordinates_end_effector_frame_homogeneous = np.dot(transformation_matrix, object_coordinates_camera_frame_homogeneous)
-
-        # Remove the homogeneous coordinate (1) from the result
-        object_coordinates_end_effector_frame = object_coordinates_end_effector_frame_homogeneous[:-1]
-
-        return object_coordinates_end_effector_frame
 
 if __name__ == '__main__':
     try:
